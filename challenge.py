abbf557b538b315879a18c2bea8b73915af4f8b1
Anthony K <antonyk@users.noreply.github.com>
2020-06-15 18:55:36 -0400

    added monday challenge and guided algo to repo

---
+++ challenge.py
@@ -0,0 +1,14 @@
+
+
+arr = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+div = 3
+# output = []
+
+for item in arr:
+  if (item % div) == 0:
+    print(item)
+    # output.append(item)
+
+# for item in output:
+#   print(item)
+
